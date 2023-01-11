8a42c48634d106682b1e88ecbd94839e5feb4cad
artslob <artslob@yandex.ru>
2023-01-11 21:17:05 +0700

    start ssh agent

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,8 @@
     gparted
   ];
 
+  programs.ssh.startAgent = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
