a65412eed9612188ab9017514998aeb7d1724ba7
artslob <artslob@yandex.ru>
2023-02-11 23:46:50 +0700

    replace sddm with default nixos display manager

--- configuration.nix
+++ configuration.nix
@@ -24,8 +24,7 @@ in {
 
   services.xserver = {
     enable = true;
-
-    displayManager.sddm.enable = true;
+    displayManager.defaultSession = "none+i3";
     desktopManager.xterm.enable = false;
     windowManager.i3 = {
       enable = true;
