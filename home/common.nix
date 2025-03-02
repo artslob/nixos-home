ab19ecbb8fd176f5014439962d1e1661a9958fed
artslob <artslob@yandex.ru>
2025-03-02 13:26:39 +0700

    use i3 config

--- home/common.nix
+++ home/common.nix
@@ -1,4 +1,4 @@
-{ pkgs, hostConfig, dotfiles, ... }: {
+{ pkgs, lib, hostConfig, dotfiles, config, ... }: {
   home.stateVersion = hostConfig.stateVersion;
 
   programs.bash = {
@@ -121,8 +121,83 @@
   xsession.windowManager.i3 = {
     enable = true;
     package = pkgs.i3-gaps;
-    config = null;
-    extraConfig = builtins.readFile "${dotfiles}/i3/.config/i3/config";
+    config = {
+      modifier = "Mod4";
+      fonts = {
+        names = [ "JetBrains Mono" ];
+        size = 9.0;
+      };
+      gaps = {
+        inner = 12;
+        smartGaps = true;
+      };
+      keybindings =
+        let modifier = config.xsession.windowManager.i3.config.modifier;
+        in lib.mkOptionDefault {
+          "${modifier}+Return" = "exec exec alacritty";
+          "${modifier}+Shift+q" = "kill";
+          "${modifier}+d" = "exec rofi -show run";
+          "${modifier}+Left" = "focus left";
+          "${modifier}+Down" = "focus down";
+          "${modifier}+Up" = "focus up";
+          "${modifier}+Right" = "focus right";
+          "${modifier}+Shift+Left" = "move left";
+          "${modifier}+Shift+Down" = "move down";
+          "${modifier}+Shift+Up" = "move up";
+          "${modifier}+Shift+Right" = "move right";
+          "${modifier}+h" = "split h";
+          "${modifier}+v" = "split v";
+          "${modifier}+f" = "fullscreen toggle";
+          "${modifier}+s" = "layout stacking";
+          "${modifier}+w" = "layout tabbed";
+          "${modifier}+e" = "layout toggle split";
+          "${modifier}+Shift+space" = "floating toggle";
+          "${modifier}+space" = "focus mode_toggle";
+          "${modifier}+a" = "focus parent";
+          "${modifier}+1" = "workspace 1";
+          "${modifier}+2" = "workspace 2";
+          "${modifier}+3" = "workspace 3";
+          "${modifier}+4" = "workspace 4";
+          "${modifier}+5" = "workspace 5";
+          "${modifier}+6" = "workspace 6";
+          "${modifier}+7" = "workspace 7";
+          "${modifier}+8" = "workspace 8";
+          "${modifier}+9" = "workspace 9";
+          "${modifier}+0" = "workspace 0";
+          "${modifier}+Shift+1" = "move container to workspace 1";
+          "${modifier}+Shift+2" = "move container to workspace 2";
+          "${modifier}+Shift+3" = "move container to workspace 3";
+          "${modifier}+Shift+4" = "move container to workspace 4";
+          "${modifier}+Shift+5" = "move container to workspace 5";
+          "${modifier}+Shift+6" = "move container to workspace 6";
+          "${modifier}+Shift+7" = "move container to workspace 7";
+          "${modifier}+Shift+8" = "move container to workspace 8";
+          "${modifier}+Shift+9" = "move container to workspace 9";
+          "${modifier}+Shift+0" = "move container to workspace 0";
+          "${modifier}+Shift+c" = "reload";
+          "${modifier}+Shift+r" = "restart";
+          "${modifier}+Shift+e" = ''
+            exec "i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit'"'';
+          "${modifier}+Tab" = "workspace back_and_forth";
+          "${modifier}+Next" = "workspace next_on_output";
+          "${modifier}+Prior" = "workspace prev_on_output";
+          "${modifier}+l" = "exec i3lock -fe";
+          "${modifier}+bracketleft" = ''exec --no-startup-id "dunstctl close"'';
+          "${modifier}+bracketright" =
+            ''exec --no-startup-id "dunstctl close"'';
+        };
+      startup = [{
+        command = "systemctl --user restart polybar";
+        always = true;
+        notification = false;
+      }];
+    };
+    # extraConfig = builtins.readFile "${dotfiles}/i3/.config/i3/config";
+    extraConfig = ''
+      default_border pixel 3
+      default_floating_border pixel 3
+      hide_edge_borders smart
+    '';
   };
 
   # to read logs: journalctl --user -xeu polybar
