39e96eb8b28153b73313b64226afffc882a7a569
artslob <artslob@yandex.ru>
2025-04-17 23:29:52 +0700

    do not change adapter

--- home/loq.nix
+++ home/loq.nix
@@ -1,7 +1,4 @@
 { lib, ... }: {
   imports = [ ./common ];
-  services.polybar.settings."module/battery" = {
-    battery = lib.mkForce "BAT1";
-    adapter = lib.mkForce "ACAD";
-  };
+  services.polybar.settings."module/battery".battery = lib.mkForce "BAT1";
 }
