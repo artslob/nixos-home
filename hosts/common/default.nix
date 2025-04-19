78951f8a10e4e0c871f547bd96ac0404bed3f817
artslob <artslob@yandex.ru>
2025-04-19 19:50:35 +0300

    enable bluetooth

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -135,6 +135,12 @@
     jetbrains-mono
   ];
 
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
