83f914a0f704e139bd027fa572fd802454a90a30
artslob <artslob@yandex.ru>
2025-04-25 22:26:25 +0300

    add conditional include to git

--- home/common/default.nix
+++ home/common/default.nix
@@ -101,5 +101,17 @@
       lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
       lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
     };
+    includes = [{
+      condition = "gitdir:~/onside/";
+      contents = {
+        user = {
+          name = "artem.s";
+          email = "artem.s@onside.io";
+          signingkey = "~/onside-keys/id_ed25519.pub";
+        };
+        gpg.format = "ssh";
+        commit.gpgsign = true;
+      };
+    }];
   };
 }
