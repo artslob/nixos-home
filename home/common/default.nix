628c15483b9d9f3e55c782bc7aa9972472ea0b14
artslob <artslob@yandex.ru>
2025-06-07 13:17:56 +0300

    move git config to separate file

--- home/common/default.nix
+++ home/common/default.nix
@@ -1,5 +1,5 @@
 { pkgs, lib, hostConfig, config, ... }: {
-  imports = [ ./alacritty.nix ./i3.nix ./polybar.nix ];
+  imports = [ ./alacritty.nix ./git.nix ./i3.nix ./polybar.nix ];
 
   home.stateVersion = hostConfig.stateVersion;
 
@@ -78,48 +78,5 @@
     desktop = null;
     publicShare = null;
     templates = null;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "artslob";
-    userEmail = "artslob@yandex.ru";
-    extraConfig = {
-      core.editor = "vim";
-      core.quotePath = false;
-      core.sshCommand = "ssh -i ~/.ssh/id_ed25519";
-      init.defaultBranch = "main";
-      user.signingkey = "~/.ssh/id_ed25519.pub";
-      gpg.format = "ssh";
-      commit.gpgsign = true;
-    };
-    aliases = {
-      a = "add";
-      au = "add -u";
-      c = "commit";
-      ca = "commit -a";
-      ch = "checkout";
-      d = "diff";
-      dc = "diff --cached";
-      l = "log";
-      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
-      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
-      pu = "push";
-      s = "status";
-      sw = "switch";
-    };
-    includes = [{
-      condition = "gitdir:~/onside/";
-      contents = {
-        user = {
-          name = "artem.s";
-          email = "artem.s@onside.io";
-          signingkey = "~/.ssh/onside/id_ed25519.pub";
-        };
-        core.sshCommand = "ssh -i ~/.ssh/onside/id_ed25519";
-        gpg.format = "ssh";
-        commit.gpgsign = true;
-      };
-    }];
   };
 }
---
+++ home/common/git.nix
@@ -0,0 +1,44 @@
+{ ... }: {
+  programs.git = {
+    enable = true;
+    userName = "artslob";
+    userEmail = "artslob@yandex.ru";
+    extraConfig = {
+      core.editor = "vim";
+      core.quotePath = false;
+      core.sshCommand = "ssh -i ~/.ssh/id_ed25519";
+      init.defaultBranch = "main";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+    };
+    aliases = {
+      a = "add";
+      au = "add -u";
+      c = "commit";
+      ca = "commit -a";
+      ch = "checkout";
+      d = "diff";
+      dc = "diff --cached";
+      l = "log";
+      lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+      lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+      pu = "push";
+      s = "status";
+      sw = "switch";
+    };
+    includes = [{
+      condition = "gitdir:~/onside/";
+      contents = {
+        user = {
+          name = "artem.s";
+          email = "artem.s@onside.io";
+          signingkey = "~/.ssh/onside/id_ed25519.pub";
+        };
+        core.sshCommand = "ssh -i ~/.ssh/onside/id_ed25519";
+        gpg.format = "ssh";
+        commit.gpgsign = true;
+      };
+    }];
+  };
+}
