7fa077318aaff7a50ad0348306afc632fc7b6c3c
artslob <artslob@yandex.ru>
2026-05-11 18:26:31 +0300

    enable wireguard and sshd

---
+++ hosts/loq/wireguard.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }: {
+  networking.wg-quick.interfaces.wg0 = {
+    address = [ "10.0.0.2/24" "fdc9:281f:04d7:9ee9::2/64" ];
+    privateKeyFile = "/etc/wireguard/loq-private-key";
+
+    peers = [{
+      publicKey = "badrGTyvIsmQUK1k2rjSUxfInUdJXjWzsq0qm86Df3E=";
+      endpoint = "artslob.me:51820";
+      allowedIPs = [ "10.0.0.0/24" "fdc9:281f:04d7:9ee9::/64" ];
+      persistentKeepalive = 25;
+    }];
+  };
+}
