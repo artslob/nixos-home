b6430612f7a8c4a30234c37571a9b23a4bb667d7
artslob <artslob@yandex.ru>
2026-08-29 14:01:31 +0300

    allow external ssh key

--- hosts/loq/wireguard.nix
+++ hosts/loq/wireguard.nix
@@ -28,5 +28,6 @@
   users.users.artslob.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILWGIVgIJ4oTrlQo3C8KfSrX0JmUP31byr8cIYozmX5A"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKfn6l7/F8vGBD5BbAzs6CLkpz/9kJA8NAfmbrlnG6E"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALE7m9huHgRQJpAirBTmDYVM/FhTR0gUpW95M11t3Qh"
   ];
 }
